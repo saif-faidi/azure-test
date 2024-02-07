b86ee28bee3ac35ca3ce93966c120057ded1620e
saif-faidi <saiffaidi28@gmail.com>
2024-02-07 18:51:22 +0100

    fix import

---
+++ tests/unit/example/test_unit_calculator.py
@@ -0,0 +1,14 @@
+# tests/unit/test_unit_calculator.py
+
+import pytest
+from src.example.calculator import Calculator
+
+@pytest.fixture
+def calculator():
+    return Calculator()
+
+def test_add(calculator):
+    assert calculator.add(1, 2) == 3
+
+def test_subtract(calculator):
+    assert calculator.subtract(5, 2) == 3
